44e373c99baeb522ffb4ea80285617fc4b92baa7
AliReza Boroujerdian <boroujerdian.alireza@hotmail.com>
2024-11-20 17:21:28 +0330

    fix: change comment project.

---
+++ S002_Comment/p001_comment/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    
+    println!("Hello, world!"); // This is my first comment.
+
+    // I Love Rust.
+
+    /*
+        This is a block comment.
+    */
+
+    /*
+     *
+     *  This is also a block comment.
+     *
+     */
+}
