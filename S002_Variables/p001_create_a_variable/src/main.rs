42244216d6b3d162eb918f1a2baa76b0612887a7
AliReza Boroujerdian <boroujerdian.alireza@hotmail.com>
2024-11-20 16:36:33 +0330

    add create a variable project

---
+++ S002_Variables/p001_create_a_variable/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let a: i32 = 5;
+    let b = 12;
+
+    let c = true;
+
+    let d: char = 'a';
+
+    let e = "hello";
+
+    let f: String = "hello".to_owned();
+    let g = String::from("test");
+}
