f499010ff2613a98f75a8b627e90404a6fb0c758
AliReza Boroujerdian <boroujerdian.alireza@hotmail.com>
2024-11-20 20:34:29 +0330

    add type suffix project

---
+++ S003_Variables/p004_type_suffix/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let a = 12;
+    let b = 13_i8;
+    let c = 14_u64;
+
+    let d = 12_530;
+    let e = 12_530.1;
+    let f = 1_2_3_4;
+
+    let g = 0xe; // Hex => e => 14
+    let h = 0o10; // Octal => 10 => 8
+    let i = 0b0001_0000; // Binary => 00010000 => 16
+
+    let j = b'A'; // Byte => 'A' => 65
+}
