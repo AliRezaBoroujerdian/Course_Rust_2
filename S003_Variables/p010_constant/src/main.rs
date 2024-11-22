6c7e3b77253f6fcbf829ba1e0d46454906d20428
AliReza Boroujerdian <boroujerdian.alireza@hotmail.com>
2024-11-22 23:32:47 +0330

    add p010_constant project

---
+++ S003_Variables/p010_constant/src/main.rs
@@ -0,0 +1,22 @@
+const MY_CONSTANT_VALUE: i32 = 12;
+
+fn main() {
+    let a: i32 = 345;
+
+    const b: i32 = 256;
+
+    println!("{a}");
+    println!("{b}");
+
+    let c: i32;
+    c = 77;
+
+    // error
+    //const d: i32;
+    //d = 18;
+
+    // error
+    //const mut e: i32 = 16;
+
+    println!("{MY_CONSTANT_VALUE}");
+}
