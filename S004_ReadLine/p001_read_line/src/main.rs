dd87f9f5a473366109b5b8df4f1d197a26d8c87d
AliReza Boroujerdian <boroujerdian.alireza@hotmail.com>
2024-11-25 13:06:18 +0330

    Update read_line project

--- S004_ReadLine/p001_read_line/src/main.rs
+++ S004_ReadLine/p001_read_line/src/main.rs
@@ -1,21 +1,21 @@
 fn main() {
     println!("username: ");
-
-    let mut username: String = String::new();
+    let mut input: String = String::new();
 
     /*
      *
      * std => standard library
      * io => input/output
-     *
      */
-    std::io::stdin().read_line(&mut username);
+    std::io::stdin().read_line(&mut input);
 
-    println!("input => {username}");
+    println!("input => {input}");
+
+    ////////////
 
     println!("password: ");
     let mut password: String = String::new();
     std::io::stdin().read_line(&mut password);
 
-    println!("input => {password}");
+    println!("input 2 => {password}");
 }
--- S004_ReadLine/p002_read_line_int/src/main.rs
+++ S004_ReadLine/p002_read_line_int/src/main.rs
@@ -1,41 +1,38 @@
 fn main() {
     println!("number: ");
 
-    let mut input_1: String = String::new();
-    std::io::stdin().read_line(&mut input_1);
+    let mut input: String = String::new();
+    std::io::stdin().read_line(&mut input);
 
-    let number: i32 = input_1
+    let number: i32 = input
         .trim() // ignore whitespace
-        .parse() // convert to integers
+        .parse() // convert to int
         .expect("error");
 
-    let another_number = number + 10;
+    println!("{number}");
+    println!("{}", number + 10);
 
-    println!("{another_number}");
-
-    ///////////////////
+    ////////////////////////
 
     println!("number 2: ");
 
     let mut input_2: String = String::new();
     std::io::stdin().read_line(&mut input_2);
 
-    let number: i16 = input_2.trim().parse().expect("error");
+    let number: i8 = input_2.trim().parse().expect("error");
 
-    let another_number: i16 = number + 10;
+    println!("{number}");
+    println!("{}", number + 10);
 
-    println!("{another_number}");
+    ////////////////////////
 
-    ///////////////////
-
-    println!("number 3: ");``
+    println!("number 3: ");
 
     let mut input_3: String = String::new();
     std::io::stdin().read_line(&mut input_3);
 
-    let number: u64 = input_3.trim().parse::<u64>().expect("Input not an integer");
+    let number: u64 = input_3.trim().parse::<u64>().expect("error");
 
-    let another_number: u64 = number + 10;
-
-    println!("{another_number}");
+    println!("{number}");
+    println!("{}", number + 10);
 }
