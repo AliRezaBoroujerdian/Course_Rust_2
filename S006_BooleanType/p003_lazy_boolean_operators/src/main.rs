d270f124f301a3bc8a7081fd4eb91999f10fcdb5
AliReza Boroujerdian <boroujerdian.alireza@hotmail.com>
2024-11-25 15:55:03 +0330

    Update lazy boolean

--- S006_BooleanType/p003_lazy_boolean_operators/src/main.rs
+++ S006_BooleanType/p003_lazy_boolean_operators/src/main.rs
@@ -2,26 +2,22 @@ fn main() {
     let b_true = true;
     let b_false = false;
 
-    println!("true  ||   true    =>  {}", b_true || b_true);
-    println!("true  &&   true    =>  {}", b_true && b_true);
+    println!("true  |   true    =>  {}", b_true || b_true); // true
+    println!("true  &   true    =>  {}", b_true && b_true); // true
 
     // error
-    //println!("true  ^^   true    =>  {}", b_true ^^ b_true);
-
-    println!("=====================");
+    //println!("true  ^   true    =>  {}", b_true ^^ b_true); // false
 
     //panic!();
 
-    // let result = b_true | panic!();
-    // println!("{result}");
+    // error
+    //let result = b_true | panic!();
 
     let result = b_true || panic!();
     println!("{result}");
 
-    println!("=====================");
-
-    // let result = b_false & panic!();
-    // println!("{result}");
+    // error
+    //let result = b_false & panic!();
 
     let result = b_false && panic!();
     println!("{result}");
