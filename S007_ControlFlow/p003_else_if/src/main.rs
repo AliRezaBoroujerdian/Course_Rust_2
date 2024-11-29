554fa8926107f210042b85ab4193afd52526bbd6
AliReza Boroujerdian <boroujerdian.alireza@hotmail.com>
2024-11-30 01:28:00 +0330

    Add p003_else_if project

---
+++ S007_ControlFlow/p003_else_if/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let number = 6;
+
+    if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else {
+        println!("number is not divisible by 2 or 3");
+    }
+}
