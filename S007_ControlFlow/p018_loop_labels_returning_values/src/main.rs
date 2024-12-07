27f1253b2a638728ac7403eb162d86ab61e29a2a
AliReza Boroujerdian <boroujerdian.alireza@hotmail.com>
2024-12-08 01:49:11 +0330

    Add p018_loop_labels_returning_values

---
+++ S007_ControlFlow/p018_loop_labels_returning_values/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let result = 'first_loop: loop {
+        println!("Loop 1");
+        loop {
+            println!("Loop 2");
+            loop {
+                println!("Loop 3");
+                loop {
+                    println!("Loop 4");
+                    break 'first_loop 6;
+                }
+            }
+        }
+    }; // ;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    println!("{result}");
+}
