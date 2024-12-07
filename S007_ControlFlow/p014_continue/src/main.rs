1dff28e8818bfb441077164ce0506ab089b78395
AliReza Boroujerdian <boroujerdian.alireza@hotmail.com>
2024-12-08 00:54:57 +0330

    Add p014_continue

---
+++ S007_ControlFlow/p014_continue/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut number = 0;
+    while number <= 100 {
+        if number % 12 != 0 {
+            number += 1;
+            continue;
+        }
+
+        println!("{number}");
+        number += 1;
+    }
+}
