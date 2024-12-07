b8e5f3a2863970e2f7f9759b34f173bd36273520
AliReza Boroujerdian <boroujerdian.alireza@hotmail.com>
2024-12-07 22:21:47 +0330

    Add p010_while

---
+++ S007_ControlFlow/p009_while/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    println!("begin");
+
+    // while true {
+    //     println!("again!");
+    // }
+
+    // while false {
+    //     println!("again!");
+    // }
+
+    let mut counter = 0;
+
+    while counter < 5 {
+        println!("again!");
+        counter += 1; //counter = counter + 1;
+    }
+
+    println!("end");
+}
