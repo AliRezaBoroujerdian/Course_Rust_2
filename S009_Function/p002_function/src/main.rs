43bebab92f6c2b01de9228fcad1c1240d7094716
AliReza Boroujerdian <boroujerdian.alireza@hotmail.com>
2024-12-15 20:48:35 +0330

    Add p2 function

---
+++ S009_Function/p002_function/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    println!("begin");
+
+    say_hello();
+    say_hello();
+    say_hello();
+
+    println!("end");
+}
+
+fn say_hello() {
+    // println!("Hello");
+    println!("Hi!");
+}
