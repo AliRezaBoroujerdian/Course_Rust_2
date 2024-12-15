b7cc27e5eb5685601bd05207da0dadf69db13fd9
AliReza Boroujerdian <boroujerdian.alireza@hotmail.com>
2024-12-15 23:41:26 +0330

    Add function p005

---
+++ S009_Function/p005_function_return/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let result = add(10, 15);
+    println!("{result}");
+}
+
+// fn add(x: i32, y: i32) -> i32 {
+//     let result = x + y;
+//     return result;
+// }
+
+// fn add(x: i32, y: i32) -> i32 {
+//     let result = x + y;
+//     result
+// }
+
+// fn add(x: i32, y: i32) -> i32 {
+//     return x + y;
+// }
+
+fn add(x: i32, y: i32) -> i32 {
+    x + y
+}
