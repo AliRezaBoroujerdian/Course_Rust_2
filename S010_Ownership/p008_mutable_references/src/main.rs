903edf293aa38c97b2b5dd421b54301ea8a1a7bd
AliReza Boroujerdian <boroujerdian.alireza@hotmail.com>
2024-12-20 21:20:30 +0330

    Add p008_mutable_references

---
+++ S010_Ownership/p008_mutable_references/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let mut s: String = String::from("Rust");
+
+    //let r1 = &mut s;
+    //let r2 = &mut s;
+
+    // s = String::from("value");
+    // println!("{}", s);
+
+    //println!("{}", r1);
+    //println!("{}", r2);
+    //println!("{}, {}", r1, r2);
+}
