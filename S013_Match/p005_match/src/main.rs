5297b2f8a04e860df14509933abb78c2cbfd8f7d
AliReza Boroujerdian <boroujerdian.alireza@hotmail.com>
2025-01-11 22:23:05 +0330

    Update match

--- S013_Match/p005_match/src/main.rs
+++ S013_Match/p005_match/src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     match age {
         1 => println!("Happy 1st Birthday!"),
         13..=19 => println!("You are a teenager!"),
+        21 | 22 => println!("You are 21 or 22 years old!"),
         x => print_age(x),
     }
 }
